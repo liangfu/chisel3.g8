b86ab6e815bee030b11dab0a12a950ed6586efd2
Liangfu Chen <liangfu.chen@icloud.com>
2019-09-21 14:57:43 +0800

    replace $ with \$;

--- lib/src/main/scala/config/Config.scala
+++ lib/src/main/scala/config/Config.scala
@@ -12,7 +12,7 @@ abstract class View {
   final def apply[T](pname: Field[T]): T = apply(pname, this)
   final def apply[T](pname: Field[T], site: View): T = {
     val out = find(pname, site)
-    require (out.isDefined, s"Key ${pname} is not defined in Parameters")
+    require (out.isDefined, s"Key \${pname} is not defined in Parameters")
     out.get
   }
 
